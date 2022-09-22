720b4863505ddc89cad4cce3f80934ea0d917898
Marco Antonio Rodriguez Salinas <marcorodriguez2013@outlook.com>
2022-09-22 18:05:20 -0500

    22_09_2022

--- pages/ejemplo/index.js
+++ pages/ejemplo/index.js
@@ -1,21 +1,28 @@
-import React, { useEffect, useState } from "react"
-import { io } from "socket.io-client"
-const socket =  io("http://localhost:3001")
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
-const Ejemplo = () => {
-    
+const socket = io("http://localhost:3001");
 
-    useEffect(() => {
-        socket.on("mensaje", (data) => {
-          alert(data);
-        })
-      },[])
-    return (
-        <>
-            <h1>Ejemplo</h1>
-           
-        </>
-    )
+const App = () => {
+
+  const [fecha, setFecha] = useState('')
+  useEffect(() => {
+    const messageListener = (args) => {
+      setFecha(args)
+    }
+    socket.on('message', messageListener)
+    return () => {
+      socket.off('message', messageListener);
+
+    }
+  }, [socket])
+
+  return (
+    <div>
+      <h1>Socket.io</h1>
+      <h2>Fecha: {fecha}</h2>
+    </div>
+  );
 }
 
-export default Ejemplo
+export default App;
